f2e68c32896b6ec17d2189f9ddf7f9a1a363028a
piphi2 <piphi2@gmail.com>
2025-05-13 15:14:25 +0200

    Create structure_programme_python.py

---
+++ structure_programme_python.py
@@ -0,0 +1,44 @@
+# voici comment doit être structuré votre script
+
+''' Titre du programme
+Auteur du programme
+quelques lignes d'explications ...
+que fait ce programme ? ...
+explications ...
+'''
+
+#importation des modules
+import module1
+from module2 import fonction_du_module
+
+# definition des fonctions
+def fonction_utile_1(parametres):
+    ''' docstring :
+        que fait la fonction ?
+        explication et types des paramètres de la fonctions
+        explication et types de la valeurs de sortie de la fonctions
+    '''
+    # instructions
+    return variable
+
+def fonction_utile_2(parametres):
+    ''' docstring'''
+    # instructions
+    return variable
+
+def fonction_utile_3(parametres):
+    ''' docstring'''
+    # instructions
+    return variable
+
+# definition des variables
+ma_variable1 = ...
+ma_variable2 = ...
+
+
+### programme principal
+# instructions, avec appels éventuels aux fonctions précédemment définies
+# instructions, avec appels éventuels aux fonctions précédemment définies
+# instructions, avec appels éventuels aux fonctions précédemment définies
+# instructions, avec appels éventuels aux fonctions précédemment définies
+
